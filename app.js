422fa15fb85a5dd58a051537baca09190b9c7538
zainrehan <zainrehan9150@gmail.com>
2023-02-01 09:00:00 +0500

    swagger implemented

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const vendorRoutes = require('./routes/vendorRoutes');
 const printRoutes = require('./routes/printRoutes');
 const path = require('path');
 const parser = require('body-parser');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerExpress = require('swagger-ui-express');
 
 app.use(parser.json());
 
@@ -16,12 +18,33 @@ app.use('/vendor', vendorRoutes);
 app.use('/print', printRoutes);
 app.use('/files', express.static(path.join(__dirname, 'files')));
 
+
+const option = {
+    definition : {
+        openapi : '3.0.0',
+        info : {
+            title: "PRINT IT",
+            description: "This is the complete live Documentation for the Print IT API and will be updated whenever there are any changes in the API",
+            version : '1.0.0',
+        },
+        servers : [
+            {
+                url : "localhost:3000" 
+            }
+        ]
+    },
+    apis: [
+        'routes/printRoutes.js',
+        'routes/userRoutes.js',
+        'routes/vendorRoutes.js'
+    ]
+}
+
+const swaggerSpec = swaggerJsDoc(option);
+
+app.use('/api/docs', swaggerExpress.serve, swaggerExpress.setup(swaggerSpec))
+
+
 app.listen(process.env.PORT, () => {
     console.log("The server is running at " + process.env.PORT);
 })
-
-
-
-
-
-
--- routes/printRoutes.js
+++ routes/printRoutes.js
@@ -3,9 +3,44 @@ const router = express.Router();
 const printController = require('../controllers/printController');
 
 
+
+/**
+ * @swagger
+ * /print/getPrintByVendor:
+ *  get: 
+ *      summary: This API is used to GET the Prints by vendor data to the server
+ *      description: The Request is performed by using this API.
+ *      responses:
+ *          200:
+ *              description: The print data for this vendor is displayed
+ */
 router.get('/getPrintByVendor', printController.getPrintByVendor);
+/**
+ * @swagger
+ * /print/postPrintData:
+ *  post: 
+ *      summary: This API is used to POST the Prints data to the server
+ *      description: The Request is performed by using this API.
+ *      responses:
+ *          200:
+ *              description:"Posted Successfully",
+                "success": true
+ */
 router.post('/postPrintData', printController.postPrintData);
-router.post('/updateStatus', printController.updateStatus);
+
+//router.post('/updateStatus', printController.updateStatus);
+
+
+/**
+ * @swagger
+ * /print/getByCustomer/id:
+ *  get: 
+ *      summary: This API is used to GET the Prints data for the server using Customer id
+ *      description: The Request is performed by using this API.
+ *      responses:
+ *          200:
+ *              description:"Retrieved Successfully",
+ */
 router.get('/getByCustomer/:id', printController.getDataByCustomer);
 
 
